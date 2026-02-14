fcb25c89c99e3b6554e2954ad2bb42607fce9bc9
Mati00003 <filpkrolikowski03@gmail.com>
2026-02-14 10:56:23 +0100

    Update config2.js

--- config2.js
+++ config2.js
@@ -33,24 +33,24 @@ const CONFIG = {
             nextBtn: "Dalej ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
-            yesBtn: "Yes!",                                             // Text for "Yes" button
-            noBtn: "No"                                                 // Text for "No" button
+            text: "Zostaniesz Moją Walentynką 14 lutego 2026? 🌹", // The big question!
+            yesBtn: "Tak!",                                             // Text for "Yes" button
+            noBtn: "Nie"                                                 // Text for "No" button
         }
     },
 
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Woow Kochasz mnie tak bardzo?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Nieskończona miłość! 🚀💝",              // Shows when they go past 1000%
+        normal: "Nieskończona! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Jestem najszczęśliwszą osobą na świecie! 🎉💝💖💝💓",
+        message: "Zostałaś nagrodzona prezentem, który otrzymasz przy najbliższym Naszym spotkaniu !",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
